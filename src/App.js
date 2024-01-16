b4bafdff85b606714d05132a1885ee5df064d1b8
The Passionate Programmer <wamungai10@gmail.com>
2024-01-16 14:20:13 +0300

    Main styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="p-8 dark:bg-slate-900 h-full lg:h-screen">
+        <div
+          className="bg-slate-100 dark:bg-slate-800 h-52 w-full absolute top-0 left-0 rounded-b-3xl"
+          style={{ zIndex: 0 }}
+        ></div>
+        <Header />
+      </section>
+    </>
   );
 }
 
