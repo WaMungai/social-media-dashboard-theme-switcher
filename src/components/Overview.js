08e51abf9c21301132698cc4a7c58e88b7f07876
The Passionate Programmer <wamungai10@gmail.com>
2024-01-16 15:53:57 +0300

    Refactor styling

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -19,7 +19,7 @@ function Overview() {
                 </li>
             </ul>
             <ul className='flex items-center justify-between'>
-                <li className='flex items-center text-emerald-600 font-bold text-sm'>87</li>
+                <li className='font-bold text-slate-800 text-3xl dark:text-white'>87</li>
                 <li className='flex items-center text-emerald-600 font-bold text-sm'>
                     <img src={up} alt="" className='w-3 mr-2' /> 3%
                 </li>
@@ -54,7 +54,7 @@ function Overview() {
         <article className='bg-slate-200 p-5 rounded shadow shadow-slate-50 dark:bg-slate-800 dark:shadow-slate-700 '>
             <ul className='flex items-center justify-between mb-5'>
                 <li className='text-slate-600 font-bold dark:text-slate-400'>Profile Views</li>
-                <li><img src={facebook} alt='Facebook'/></li>
+                <li><img src={instagram} alt='Instagram'/></li>
             </ul>
             <ul className='flex items-center justify-between'>
                 <li className='font-bold text-slate-800 text-3xl dark:text-white'>52k</li>
@@ -80,7 +80,7 @@ function Overview() {
         <article className='bg-slate-200 p-5 rounded shadow shadow-slate-50 dark:bg-slate-800 dark:shadow-slate-700 '>
             <ul className='flex items-center justify-between mb-5'>
                 <li className='text-slate-600 font-bold dark:text-slate-400'>Likes</li>
-                <li><img src={instagram} alt='Instagram'/></li>
+                <li><img src={twitter} alt='Twitter'/></li>
             </ul>
             <ul className='flex items-center justify-between'>
                 <li className='font-bold text-slate-800 text-3xl dark:text-white'>507</li>
